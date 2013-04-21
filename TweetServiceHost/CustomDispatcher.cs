f1f69821dae80594bf8353fec6f671fa1d90a13f
Darren Neimke <darren.neimke@gmail.com>
2013-04-21 12:24:40 +0930

    Cleaning up unneccessary code

--- TweetServiceHost/CustomDispatcher.cs
+++ TweetServiceHost/CustomDispatcher.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Xml;
-using System.Reflection;
-using System.IO;
-using System.ServiceModel;
-using System.ServiceModel.Description;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ServiceModel.Dispatcher;
 using System.ServiceModel.Channels;
-using System.ServiceModel.Configuration;
-using System.Xml.Linq;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Xml;
 
 namespace TweetServiceHost
 {
@@ -31,12 +22,6 @@ namespace TweetServiceHost
             return bodyElement;            
         }
 
-
-
-
-
-
-
         public void AddBindingParameters(ContractDescription contractDescription, ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
         {
             
@@ -54,98 +39,6 @@ namespace TweetServiceHost
         }
 
         public void Validate(ContractDescription contractDescription, ServiceEndpoint endpoint)
-        {
-            
-        }
-    }
-
-
-    public class ConsoleMessageTracer : IDispatchMessageInspector, IClientMessageInspector, IDispatchOperationSelector 
-    { 
-        private Message TraceMessage(MessageBuffer buffer) 
-        { 
-            Message msg = buffer.CreateMessage(); 
-            Console.WriteLine("\n{0}\n", msg); 
-            return buffer.CreateMessage(); 
-        }
-
-        NameValueCollection actionMap = new NameValueCollection { { "a", "SendTweet" }, { "b", "DeleteTweet" } };
-
-        public string SelectOperation(ref Message message)
-        {
-            Console.WriteLine("In QueryStringDispatcherBehavior.SelectOperation");
-            Uri uri = new Uri(message.Headers.Action);
-            string query = uri.Query;
-
-            Console.WriteLine(query);
-
-            //string methodName = actionMap[message.Headers.Action.ToString()];
-
-            //return methodName;
-
-            return "DeleteTweet";
-        }
-        
-        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext) 
-        { 
-            request = TraceMessage(request.CreateBufferedCopy(int.MaxValue)); 
-            return null; 
-        } 
-        
-        public void BeforeSendReply(ref Message reply, object correlationState) 
-        { 
-            reply = TraceMessage(reply.CreateBufferedCopy(int.MaxValue)); 
-        } 
-        
-        public void AfterReceiveReply(ref Message reply, object correlationState) 
-        { 
-            reply = TraceMessage(reply.CreateBufferedCopy(int.MaxValue)); 
-        } 
-        
-        public object BeforeSendRequest(ref Message request, IClientChannel channel) 
-        { 
-            request = TraceMessage(request.CreateBufferedCopy(int.MaxValue)); 
-            return null; 
-        } 
-    }
-
-
-    public class ConsoleMessageTracing : Attribute, IEndpointBehavior, IServiceBehavior
-    {
-        void IEndpointBehavior.ApplyClientBehavior(ServiceEndpoint endpoint, ClientRuntime clientRuntime)
-        {
-            clientRuntime.MessageInspectors.Add(new ConsoleMessageTracer());
-        }
-
-        void IEndpointBehavior.ApplyDispatchBehavior(ServiceEndpoint endpoint, EndpointDispatcher endpointDispatcher)
-        {
-            endpointDispatcher.DispatchRuntime.MessageInspectors.Add(new ConsoleMessageTracer());
-        }
-
-        // remaining methods empty 
-        void IServiceBehavior.ApplyDispatchBehavior(ServiceDescription desc, ServiceHostBase host)
-        {
-            foreach (ChannelDispatcher cDispatcher in host.ChannelDispatchers)
-                foreach (EndpointDispatcher eDispatcher in cDispatcher.Endpoints)
-                    eDispatcher.DispatchRuntime.MessageInspectors.Add(new ConsoleMessageTracer());
-        }
-
-        public void AddBindingParameters(ServiceEndpoint endpoint, BindingParameterCollection bindingParameters)
-        {
-            
-        }
-
-        public void Validate(ServiceEndpoint endpoint)
-        {
-            
-        }
-
-        public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
-        {
-            
-        }
-
-        public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
         {
             
         }
